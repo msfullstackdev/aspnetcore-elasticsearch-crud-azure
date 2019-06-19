64a83bd1bc951979f40c198365faf7da0c8d4ba3
Tamilselvan Sellappan <t.sellappan@perficient.com>
2019-06-20 00:43:06 +0530

    added msi for accessing key from key vault without clientid and secret

--- CS.API/Program.cs
+++ CS.API/Program.cs
@@ -5,7 +5,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Logging;
 
 namespace CS.API
@@ -20,6 +23,25 @@ namespace CS.API
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 //.UseApplicationInsights()
+                .ConfigureAppConfiguration((context,config)=>
+                {
+                    if(context.HostingEnvironment.IsProduction())
+                    {
+                        var builtConfig = config.Build();
+
+                        var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                        var keyVaultClient = new KeyVaultClient(
+                            new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                        config.AddAzureKeyVault(
+                                 $"https://{builtConfig["KeyVaultName"]}.vault.azure.net/",
+                                 keyVaultClient,
+                                 new DefaultKeyVaultSecretManager());
+
+                    }
+
+
+                })
                 .UseStartup<Startup>();
     }
 }
