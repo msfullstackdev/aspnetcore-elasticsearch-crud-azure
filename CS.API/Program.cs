65510fe07f3782d38237583a8f3c210f1dafd15e
Tamilselvan Sellappan <t.sellappan@perficient.com>
2019-06-19 18:47:24 +0530

    Added Application Insights telemetry

--- CS.API/Program.cs
+++ CS.API/Program.cs
@@ -19,6 +19,7 @@ namespace CS.API
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
